927d853ae0e433d46079ec539d909677a9e7923c
Yin Yin Wu-Hanke <Y.Wu-Hanke@ellusion.de>
2019-08-15 09:21:15 +0200

    navi active

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
-$(document).ready(function () {
-    var url = window.location;
-    $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
-    $('ul.nav a').filter(function() {
-         return this.href == url;
-    }).parent().addClass('active');
-});
+<script type="text/javascript">
+    $(document).ready(function () {
+        var url = window.location;
+        $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
+        $('ul.nav a').filter(function() {
+             return this.href == url;
+        }).parent().addClass('active');
+    });
+</script> 
