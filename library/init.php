266ade8b4dd7f8adb8ac38b9c0b2fd00bd9e5e7e
Mark Paspirgilis <service@html42.de>
2019-08-20 22:09:38 +0200

    basic constants for Directories

--- library/init.php
+++ library/init.php
@@ -1,2 +1,9 @@
 <?php
-include __DIR__ . DIRECTORY_SEPARATOR . 'functions.php';
+
+//Define Constants
+define('DIR_LIB', str_replace('\\', '/', __DIR__) . '/');
+define('ROOT', str_Replace('library/', '', DIR_LIB));
+define('DIR_BACKEND', ROOT . '_backend/');
+define('DIR_BACKEND_CLASSES', DIR_BACKEND . 'classes/');
+
+include DIR_LIB . 'functions.php';
