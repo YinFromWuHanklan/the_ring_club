8ee11f10bd379e917b0af6b16d9bcbe08bea1ad5
Mark Paspirgilis <service@html42.de>
2019-09-01 14:01:55 +0200

    logo

--- components/navbar.php
+++ components/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark sticky-top">
     <a class="navbar-brand" href="index.html">
-        <img src="images/thering-logo-navbar.png" alt="The Ring" width="90" />
+        <img src="images/thering-logo.png" alt="The Ring" width="90" />
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
