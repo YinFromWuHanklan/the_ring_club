27eced94acad40c795aed35732a37e3c6db8e886
Yin Yin Wu-Hanke <Y.Wu-Hanke@ellusion.de>
2019-09-17 22:16:45 +0200

    personaltraining page

---
+++ pages/personaltraining.php
@@ -0,0 +1,24 @@
+<?php
+$website_title = 'Personaltraining | The Ring Boxing Club';
+$website_description = 'Personaltraining | The Ring Boxing Club';
+include '../components/html_top.php';
+?>
+
+<div class="row">
+    <div class="col-md-2">
+
+    </div>
+    <div class="my-5 col-md-8">
+
+        <h1>Personaltraining</h1>
+        <h2 class="text-center">
+            Boxen
+        </h2>
+        
+    </div>
+    <div class="col-md-2">
+
+    </div>
+</div>
+
+<?php include ROOT . "components/html_bottom.php"; ?>
