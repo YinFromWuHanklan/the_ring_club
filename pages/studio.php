7464feb8c44c17eb4934d22f5a65b29f25e8f08b
Yin Yin Wu-Hanke <Y.Wu-Hanke@ellusion.de>
2020-01-11 07:20:47 +0100

    slideshow desktop

--- pages/studio.php
+++ pages/studio.php
@@ -1,7 +1,7 @@
 <?php
 $website_title = 'Studio';
 $website_description = 'Studio';
-include '../components/html_top.php';
+include '../components/html_top_no_header.php';
 ?>
 
 <div class="row">
@@ -11,41 +11,44 @@ include '../components/html_top.php';
     <div class="m-5 col-md-8">
 
         <h1>Studio</h1>
+        <?php include '../components/slideshow.html' ?>
     </div>
     <div class="col-md-2">
 
     </div>
 </div>
 
-
+<!-- 
 <div class="container">
-    <div class="row" style="height:">
-        <div class="col-md-8">
-            <img src="assets/images/thering-img-01.jpg" alt="The Ring Boxing Club Studio" />
-        </div>
-        <div class="col-md-4">
-            <img src="assets/images/thering-img-02.jpg" alt="The Ring Boxing Club Studio" style="height: 600px" />      
+    <div class="row">
+        <div class="col-md-12">
+            <img src="assets/images/thering-img-01.jpg" alt="The Ring Boxing Club Studio" width="960" />
         </div>
     </div>
     <div class="row">
-        <div class="col-md-4">
-            <img src="assets/images/thering-img-03.jpg" alt="The Ring Boxing Club Studio" style="height: 600px" />
+        <div class="col-md-4 p-0">
+            <img src="assets/images/thering-img-02.jpg" alt="The Ring Boxing Club Studio" width="320" />      
         </div>
-        <div class="col-md-4">
-            <img src="assets/images/thering-img-04.jpg" alt="The Ring Boxing Club Studio" style="height: 600px" />      
+        <div class="col-md-4 p-0">
+            <img src="assets/images/thering-img-03.jpg" alt="The Ring Boxing Club Studio" width="320" />
         </div>
-        <div class="col-md-4">
-            <img src="assets/images/thering-img-05.jpg" alt="The Ring Boxing Club Studio" style="height: 600px" />      
-        </div>
-    </div>
-    <div class="row">     
-        <div class="col-md-4">
-            <img src="assets/images/thering-img-06.jpg" alt="The Ring Boxing Club Studio" style="height: 600px" />
-        </div>
-        <div class="col-md-8">
-            <img src="assets/images/thering-img-07.jpg" alt="The Ring Boxing Club Studio" style="height: 600px" />      
+        <div class="col-md-4 p-0">
+            <img src="assets/images/thering-img-04.jpg" alt="The Ring Boxing Club Studio" width="320" />      
         </div>
     </div>
-</div>
+    <div class="row">
+        <div class="col-md-6 p-0">
+            <img src="assets/images/thering-img-05.jpg" alt="The Ring Boxing Club Studio" width="480" />      
+        </div>     
+        <div class="col-md-6 p-0">
+            <img src="assets/images/thering-img-06.jpg" alt="The Ring Boxing Club Studio" width="480" />
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <img src="assets/images/thering-img-07.jpg" alt="The Ring Boxing Club Studio" width="960" />      
+        </div>
+    </div>
+</div> -->
 
 <?php include ROOT . "components/html_bottom.php"; ?>
