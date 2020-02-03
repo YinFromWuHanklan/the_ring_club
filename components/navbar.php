cf393dda46bdb509ea9245f31cf9433ade1bb165
Yin Yin Wu-Hanke <Y.Wu-Hanke@ellusion.de>
2020-02-03 09:56:55 +0100

    navbar dropdown comment

--- components/navbar.php
+++ components/navbar.php
@@ -9,6 +9,17 @@
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto">
+            <!-- ToDo: Navbar mit Dropdown
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Dropdown
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                </div>
+            </li>
+            -->
             <?php
             foreach (array(
         'studio.html' => 'Studio',
